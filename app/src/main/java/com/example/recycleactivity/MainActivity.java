3cd8d797dcd225b823b82b00e63396a160312db1
Tamer-Mansour <78074218+Tamer-Mansour@users.noreply.github.com>
2021-07-06 11:59:33 +0300

    the new update

--- app/src/main/java/com/example/recycleactivity/MainActivity.java
+++ app/src/main/java/com/example/recycleactivity/MainActivity.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     private ArrayList<ExampleItem> mExampleList;
     private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
+    private ExampleAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private Button buttonInsert;
     private Button buttonRemove;
@@ -25,6 +25,52 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         createExampleList();
         buildRecyclerView();
+
+        setButtons();
+    }
+    public void insertItem(int position) {
+        mExampleList.add(position, new ExampleItem(R.drawable.camera, "New Item At Position" + position, "This is Line 2"));
+        mAdapter.notifyItemInserted(position);
+    }
+    public void removeItem(int position) {
+        mExampleList.remove(position);
+        mAdapter.notifyItemRemoved(position);
+    }
+
+    public void changeItem(int position ,  String text){
+        mExampleList.get(position).changeText1(text);
+        mAdapter.notifyItemChanged(position);
+
+    }
+
+    public void createExampleList() {
+        mExampleList = new ArrayList<>();
+        mExampleList.add(new ExampleItem(R.drawable.camera, "Line 1", "Line 2"));
+        mExampleList.add(new ExampleItem(R.drawable.facebook, "Line 3", "Line 4"));
+        mExampleList.add(new ExampleItem(R.drawable.gmail, "Line 5", "Line 6"));
+    }
+    public void buildRecyclerView() {
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new ExampleAdapter(mExampleList) ;
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+        mAdapter.setOnItemClickListener(new ExampleAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                //mExampleList.get(position).changeText1("Clicked");
+                changeItem(position , "Clicked");
+            }
+
+            @Override
+            public void onDeleteClick(int position) {
+                removeItem(position);
+            }
+        });
+    }
+    public void setButtons(){
         buttonInsert = findViewById(R.id.button_insert);
         buttonRemove = findViewById(R.id.button_remove);
         editTextInsert = findViewById(R.id.edittext_insert);
@@ -43,28 +89,5 @@ public class MainActivity extends AppCompatActivity {
                 removeItem(position);
             }
         });
-    }
-    public void insertItem(int position) {
-        mExampleList.add(position, new ExampleItem(R.drawable.camera, "New Item At Position" + position, "This is Line 2"));
-        mAdapter.notifyItemInserted(position);
-    }
-    public void removeItem(int position) {
-        mExampleList.remove(position);
-        mAdapter.notifyItemRemoved(position);
-    }
-    public void createExampleList() {
-        mExampleList = new ArrayList<>();
-        mExampleList.add(new ExampleItem(R.drawable.camera, "Line 1", "Line 2"));
-        mExampleList.add(new ExampleItem(R.drawable.facebook, "Line 3", "Line 4"));
-        mExampleList.add(new ExampleItem(R.drawable.gmail, "Line 5", "Line 6"));
-    }
-    public void buildRecyclerView() {
-        mRecyclerView = findViewById(R.id.recyclerView);
-        mRecyclerView.setHasFixedSize(true);
-        mLayoutManager = new LinearLayoutManager(this);
-        mAdapter = new ExampleAdapter(mExampleList) ;
-
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mRecyclerView.setAdapter(mAdapter);
     }
 }
