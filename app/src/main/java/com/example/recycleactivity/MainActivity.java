11d6c4bbabac974799ac1aedb10b4c187e0e18ba
Tamer-Mansour <78074218+Tamer-Mansour@users.noreply.github.com>
2021-07-06 11:23:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/recycleactivity/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.recycleactivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+public class MainActivity extends AppCompatActivity {
+    private ArrayList<ExampleItem> mExampleList;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private Button buttonInsert;
+    private Button buttonRemove;
+    private EditText editTextInsert;
+    private EditText editTextRemove;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        createExampleList();
+        buildRecyclerView();
+        buttonInsert = findViewById(R.id.button_insert);
+        buttonRemove = findViewById(R.id.button_remove);
+        editTextInsert = findViewById(R.id.edittext_insert);
+        editTextRemove = findViewById(R.id.edittext_remove);
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = Integer.parseInt(editTextInsert.getText().toString());
+                insertItem(position);
+            }
+        });
+        buttonRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = Integer.parseInt(editTextRemove.getText().toString());
+                removeItem(position);
+            }
+        });
+    }
+    public void insertItem(int position) {
+        mExampleList.add(position, new ExampleItem(R.drawable.camera, "New Item At Position" + position, "This is Line 2"));
+        mAdapter.notifyItemInserted(position);
+    }
+    public void removeItem(int position) {
+        mExampleList.remove(position);
+        mAdapter.notifyItemRemoved(position);
+    }
+    public void createExampleList() {
+        mExampleList = new ArrayList<>();
+        mExampleList.add(new ExampleItem(R.drawable.camera, "Line 1", "Line 2"));
+        mExampleList.add(new ExampleItem(R.drawable.facebook, "Line 3", "Line 4"));
+        mExampleList.add(new ExampleItem(R.drawable.gmail, "Line 5", "Line 6"));
+    }
+    public void buildRecyclerView() {
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new ExampleAdapter(mExampleList) ;
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+}
